6d57a1e72a1c0f14d558967818325ba1daba2c46
Tadeu Granemann <tadeu@Tadeus-MacBook-Air.local>
2019-09-27 00:30:57 -0300

    Test fix png iCCP and test fix GIF

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 # compare-images
-Calculates images similarity
 
-apt install -y python3 python3-pip libsm6 libxext6 libxrender-dev
+libpng warning: iCCP: CRC error
+mogrify *.png
+convert in.png out.png
+
+
+This project prioritizes accuracy, the trade-off is more resources usage
+Images used in the test case are under GNU license (unsplash.com)
+Loads a color image. Any transparency of image will be neglected
+Ideally 224x224
+JPEG
+CSV delimiter must ;
+
+apt install -y python3 python3-pip libsm6 libxext6 libxrender-dev imagemagick
+
+https://www.imgonline.com.ua/eng/similarity-percent.php
+https://deepai.org/machine-learning-model/image-similarity
+
+# Why docker
+
+Libpng-1.6 is more stringent about checking ICC profiles than previous versions
+
+# Validation metodolgy
+
+neural network
+external tools https://www.imgonline.com.ua/eng/similarity-percent-result.php
+visual analysis 
+
+# Future versions
+
+Scale images proportionally
--- compare-images.py
+++ compare-images.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 import pandas as pd
 import numpy as np
 import keras
@@ -8,13 +9,18 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import h5py
 import cv2
+import csv
 from keras.layers import Flatten, Dense, Input,concatenate
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout
 from keras.models import Model
 from keras.models import Sequential
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 from scipy import spatial
+from PIL import Image
+
 
 # pip install "numpy<1.17"
 
@@ -22,6 +28,9 @@ vgg16 = keras.applications.VGG16(weights='imagenet', include_top=True, pooling='
 basemodel = Model(inputs=vgg16.input, outputs=vgg16.get_layer('fc2').output)
 
 def get_feature_vector(img):
+    height, width, channels = img.shape 
+    print(height)
+    sys.exit()
     img1 = cv2.resize(img, (224, 224))
     feature_vector = basemodel.predict(img1.reshape(1, 224, 224, 3))
     return feature_vector
@@ -31,18 +40,72 @@ def calculate_similarity(vector1, vector2):
 
 def read_image(filename):
     oriimg = cv2.imread(filename)
+    print(filename)
+    print(oriimg)
+    sys.exit()
     return oriimg
 
+def processImage(infile):
+    try:
+        im = Image.open(infile)
+    except IOError:
+        print("Cant load", infile)
+        sys.exit(1)
+    i = 0
+    mypalette = im.getpalette()
+
+    try:
+        while 1:
+            im.putpalette(mypalette)
+            new_im = Image.new("RGBA", im.size)
+            new_im.paste(im)
+            new_im.save('foo'+str(i)+'.png')
+
+            i += 1
+            im.seek(im.tell() + 1)
+
+    except EOFError:
+        pass # end of sequence
+
 def main():
-    img1 = read_image("./img1.jpg")
-    img2 = read_image("./img2.jpg")
-    img3 = read_image("./img3.jpg")
-    f1 = get_feature_vector(img1)
-    f2 = get_feature_vector(img2)
+
+    processImage("tests/img/img-003.gif")
+
+    #img1 = read_image("tests/img/img-001.jpeg")
+    #img2 = read_image("tests/img/img-001.jpeg")
+    img3 = read_image("tests/img/img-003.gif")
+    #f1 = get_feature_vector(img1)
+    #f2 = get_feature_vector(img2)
     f3 = get_feature_vector(img3)
+    sys.exit()
     print(calculate_similarity(f1, f2)) # 0.7384121417999268
     print(calculate_similarity(f1, f3)) # 0.48573723435401917
+    sys.exit()
+  
 
+    '''
+    for i in range(1,10):
+        img1 = read_image("tests/img/img-00"+str(i)+".jpeg")
+        f1 = get_feature_vector(img1)
+        for j in range(1,10):
+            img2 = read_image("tests/img/img-00"+str(j)+".jpeg")
+            f2 = get_feature_vector(img2)
+            print("tests/img/img-00"+str(i)+".jpeg"+" "+"tests/img/img-00"+str(j)+".jpeg")
+            print(calculate_similarity(f1, f2))
+    '''
+
+    with open('tests/input.csv') as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=';')
+        next(readCSV, None)
+        for row in readCSV:
+            img1Path = row[0]
+            img2Path = row[1]
+            img1 = read_image(img1Path)
+            img2 = read_image(img2Path)
+            f1 = get_feature_vector(img1)
+            f2 = get_feature_vector(img2)
+            print(img1Path+" "+img2Path)
+            print(calculate_similarity(f1, f2))
 
 
 if __name__ == "__main__":
