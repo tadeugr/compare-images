9fdc258f2c0b67a307d5b6aa72412a4bbb23c2ce
Tadeu Eduardo Depine Granemann <tadeu.granemann@hbsis.com.br>
2019-09-27 13:51:58 -0300

    New tests and instructions

--- README.md
+++ README.md
@@ -1,15 +1,42 @@
-# compare-images
+# Overview
+
+This project calculates the similarity of images. It reads an input CSV file containing the absolute path of images tuples that must be compared. The result is another CSV file containing the similarity score and the time elapsed for each analysis.
+
+# Methodology
+
+The algorithm uses [Keras](https://keras.io/) and [Tensorflow](https://www.tensorflow.org/).
+
+VGG16 is the chosen model, with weights pre-trained on ImageNet.
+
+That being said, this project prioritizes accuracy with a fairness tradeoff for resource allocation.
+
+# Important notes
+
+* The similarity score is a range from 0 (zero) to 1 (one). Zero being "identical" and One being "completely different". **Note that similarity scores under 30% are being considered "completely different".**
+
+* Supported files: JPEG, PNG and GIF.
+
+* GIF files are converted to PNG RBG format.
+
+* Transparency on PNG files will be neglected.
+
+* All images are rezied to 224x224 (due Keras requirement). If your image set container images much bigger or smaller, there will a slightly accuracy degradation. 
+
+* Libpng 1.6+ is more stringent about checking ICC profiles than previous versions. If your system has Libpng 1.6+ installed, you might see some messages like `libpng warning: iCCP: CRC error`. You can ignore those messages. They will not affect the analysis.
+
+* All images in the tests folder are under GNU license.
+
+
+
+virtualenv is recommended
 
 libpng warning: iCCP: CRC error
 mogrify *.png
 convert in.png out.png
 
-Note for GIF: 
+Note for GIF
 
-This project prioritizes accuracy, the trade-off is more resources usage
-Images used in the test case are under GNU license (unsplash.com)
-Loads a color image. Any transparency of image will be neglected
-Ideally 224x224
+
 
 CSV delimiter must ;
 
@@ -18,16 +45,35 @@ apt install -y python3 python3-pip libsm6 libxext6 libxrender-dev imagemagick
 https://www.imgonline.com.ua/eng/similarity-percent.php
 https://deepai.org/machine-learning-model/image-similarity
 
-# Why docker
+# Input file
 
-Libpng-1.6 is more stringent about checking ICC profiles than previous versions
+The CSV will contain 2 fields (image1 and image2) with N records. Each field contains the absolute path to an image file.
+
+| image1 |  image2 |
+|--------|---------|
+| aa.png |  ba.png |
+| ab.png |  bb.png |
+| ac.png |  ac.gif |
+| ad.png |  bd.png |
+
+# Output file
+
+The CSV file will need to have 4 fields (image1, image2, similar, elapsed - in seconds) and have the same amount of records as the input file.
+
+The values that fall under the first 2 fields (image1 and image2) need to be the same as the input file.
+
+The values that fall under the similar field will need to represent a "score" based on how similar image1 is to image2
+
+| image1 |  image2 | similar | elapsed  |
+|--------|---------| ------- | -------- |
+| aa.png |  ba.png | 0       | 0.006    |
+| ab.png |  bb.png | 0.23    | 0.843    |
+| ac.png |  ac.gif | 0       | 1.43     |
+| ad.png |  bd.png | 1       | 2.32     |
+
 
 # Validation metodolgy
 
 neural network
 external tools https://www.imgonline.com.ua/eng/similarity-percent-result.php
 visual analysis 
-
-# Future versions
-
-Multithread
--- compare-images.py
+++ compare-images.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 
+#
+# Script to calculate the similarity of N pairs of images.
+# The scripts reads an input CSV file containing the
+# absolute path of the images that will be compared.
+# 
+# The result is another CSV file containing the similarity score
+# and the time elapsed for each analysis. 
+#
+
+#
+# START import libs
+#
 import sys
 import os
 import glob
@@ -14,6 +26,9 @@ import uuid
 import logging
 import traceback
 import time
+from scipy import spatial
+from PIL import Image
+import imghdr
 
 stderr = sys.stderr
 sys.stderr = open(os.devnull, 'w')
@@ -29,14 +44,11 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
-from scipy import spatial
-from PIL import Image
-import imghdr
+#
+# END import libs
+#
 
-
-# pip install "numpy<1.17"
-
-
+# Common variables
 
 vgg16 = keras.applications.VGG16(weights='imagenet', include_top=True, pooling='max', input_shape=(224, 224, 3))
 basemodel = Model(inputs=vgg16.input, outputs=vgg16.get_layer('fc2').output)
@@ -44,14 +56,17 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 convertMap = {}
 
+# Resize and reshape image
 def getFeatureVector(img):
     img1 = cv2.resize(img, (224, 224))
     feature_vector = basemodel.predict(img1.reshape(1, 224, 224, 3))
     return feature_vector
 
+# Calculate vectors cosine distance
 def calculateCosineDistance(vector1, vector2):
     return 1 - spatial.distance.cosine(vector1, vector2)
 
+# Calculate similarity based on vector analysis
 def calculateSimilarity(img1Path, img2Path):
     img1 = readImage(img1Path)
     img2 = readImage(img2Path)
@@ -59,22 +74,32 @@ def calculateSimilarity(img1Path, img2Path):
     f2 = getFeatureVector(img2)
     return calculateCosineDistance(f1, f2)
 
+# Format a number with 2 decimal digits
 def formatNumber2d(number):
     return float("{:.2f}".format(number))
 
+# Remove trailing zeroes
 def formatNumberTrailingZero(number):
     return float("{0:g}".format(number))
 
+# Calculate the final similarity score
 def getScore(originalScore):
+    # If similarity level is lower than 30%
+    # set score to "completely diffrent"
+    if originalScore < 0.3:
+        originalScore = 0
+    
     score = 1 - originalScore
     score = "{:.2f}".format(score)
     score = "{0:g}".format(float(score))
     return score
 
+# Open and read an image
 def readImage(filename):
     img = cv2.imread(filename)
     return img
 
+# Convert GIF to PNG
 def gifToPng(infile):
     # Open GIF file
     try:
@@ -90,11 +115,11 @@ def gifToPng(infile):
     try:
         while 1:
             im.putpalette(myPalette)
-            new_im = Image.new("RGBA", im.size)
-            new_im.paste(im)
+            newIm = Image.new("RGBA", im.size)
+            newIm.paste(im)
 
             fileName = prefix+str(i)+'.png'
-            new_im.save(fileName)
+            newIm.save(fileName)
             pngFileNames.append(fileName)
 
             i += 1
@@ -111,15 +136,19 @@ def gifToPng(infile):
     # only the first GIF layer
     return pngFileNames[0]
 
+# Get image type (jpeg, png, gif)
 def getImageType(imgPath):
     return imghdr.what(imgPath)
 
+# Check supported files formart
 def isFileTypeSupported(imgPath, imgType = None):
     supportedFiles = ['jpeg', 'png', 'gif']
     if not imgType:
         imgType = getImageType(imgPath)
     return imgType in supportedFiles
 
+# Analyse image supported types
+# Convert GIF to PNG
 def formatFiles(imgPathArray):
     try:
         for imgPath in imgPathArray:
@@ -140,12 +169,14 @@ def formatFiles(imgPathArray):
         traceback.print_exc()
         sys.exit(1)
 
+# Check if an image was converted
 def getConvertMap(imgPath):
     try:
         return convertMap[imgPath]
     except KeyError:
         return imgPath
 
+# Write a file
 def saveFile(path, content):
     try:
         f = open(path, 'w' )
@@ -155,6 +186,7 @@ def saveFile(path, content):
         logger.error('ERROR: '+ str(e))
         traceback.print_exc()
 
+# Delete converted files
 def cleanUp():
     try:
         fileList = glob.glob('auto-converted-*', recursive=True)
@@ -164,15 +196,26 @@ def cleanUp():
         logger.error('ERROR: '+ str(e))
         traceback.print_exc()
 
-
+# Main function
 def main():
+    logger.debug('Start funcion main')
     try:
-        
+        logger.debug('Validating parameters')
         if not sys.argv[1]:
-            raise Exception('CSV input path not defined')
+            raise Exception('Input CSV path not defined')
+        if not sys.argv[2]:
+            raise Exception('Output CSV path not defined')
 
         csvInputPath = sys.argv[1]
-
+        csvOutputPath = sys.argv[2]
+    except IndexError:
+        logger.error('ERROR: Required parameter not set')
+        logger.error('Usage: python compare-images.py <CSV INPUT FILE PATH> <CSV OUTPUT FILE PATH>')
+        logger.error('Usage example: python compare-images.py input.csv output.csv')
+        sys.exit(1)
+    
+    try:
+        logger.debug('Opening input CSV')
         with open(csvInputPath) as csvfile:
             inputCsv = csv.reader(csvfile, delimiter=';')
             outputCsv = "image1;image2;similar;elapsed\n"
@@ -194,15 +237,18 @@ def main():
                 score = getScore(similarity)
                 endTime = time.time()
                 deltaTime = formatNumber2d(endTime - startTime)
-                logger.info('Time elapsed (seconds): %s', str(deltaTime))
 
-                outputCsv += ("%s;%s;" % (img1PathOriginal, img2PathOriginal))
-                outputCsv += ("%s;%s\n" % (score, deltaTime))
-                print(outputCsv)
+                outputCsvLine = ''
+                outputCsvLine += ("%s;%s;" % (img1PathOriginal, img2PathOriginal))
+                outputCsvLine += ("%s;%s\n" % (score, deltaTime))
+                outputCsv += outputCsvLine
+                logger.debug('Result %s', outputCsvLine)
 
-                #break
-        print(outputCsv)
+        saveFile(csvOutputPath, outputCsv)
         cleanUp()
+
+        logger.debug('End funcion main')
+        logger.info('Results saved on %s', csvOutputPath)
 
     except Exception as e:
         logger.error('ERROR: '+ str(e))
